ee5d0bf205333394ba0e065420b7ce970d0ae469
Jaimy Putter <33345988+Spraxs@users.noreply.github.com>
2021-03-30 14:40:45 +0200

    Updated TargetFollower

--- Assets/Scripts/Ship/TargetFollower.cs
+++ Assets/Scripts/Ship/TargetFollower.cs
@@ -3,29 +3,43 @@
 namespace Ship
 {
     /// <summary>
-    /// Follow target transform
+    /// Follows the given Target Transform via a Vector3.Lerp
     /// </summary>
     public class TargetFollower : MonoBehaviour 
     {
-        // Follow speed
         [SerializeField]
-        private float followSpeed;
+        // The speed to lerp towards the Target position
+        private float followSpeed = 7;
 
-        // Transform to follow
         [SerializeField]
+        // Transform of the target
         private Transform targetTransform;
 
         void FixedUpdate()
         {
-            this.LerpTowardsTarget();
+            // Follow the target by lerping towards it
+            LerpToTarget(transform, targetTransform, followSpeed);
         }
 
         /// <summary>
-        /// Lerp current transform towards target
+        /// Lerp the given Transform towards the given target Transform
         /// </summary>
-        private void LerpTowardsTarget()
+        /// <param name="_transform">Transform that needs to change</param>
+        /// <param name="_targetTransform">Target Transform that </param>
+        /// <param name="_followSpeed">Speed of the lerp towards, target</param>
+        private void LerpToTarget(Transform _transform, Transform _targetTransform, float _followSpeed)
         {
-            transform.position = Vector3.Lerp(transform.position, targetTransform.position, followSpeed * Time.deltaTime);
+            // Check if the targetTransform has been filled in
+            if (_targetTransform)
+            {
+                // Lerp the given Transform towards the given target Transform
+                _transform.position = Vector3.Lerp(_transform.position, _targetTransform.position, _followSpeed * Time.deltaTime);
+            }
+            else
+            {
+                // Else if it does not exits, log an Error
+                Debug.LogError("Camera target to follow not found");
+            }
         }
     }
 }
