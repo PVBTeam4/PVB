c976e99ae19adbe9d165a421b0585b9231411666
Thomas Linssen <31830553+ThomasGITH@users.noreply.github.com>
2021-04-01 13:06:33 +0200

    Update Bullet.cs

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls a gameobject to act as a bullet projectile.
+/// </summary>
 public class Bullet : MonoBehaviour
 {
     //Sets the speed of the projectile
@@ -17,21 +20,21 @@ public class Bullet : MonoBehaviour
     /// </summary>
     void Start()
     { 
-        destroyBullet(lifeSpan);
+        DestroyBullet(lifeSpan);
     }
 
     /// <summary>
     /// Updates the movement of the bullet for every frame of its lifespan
     /// </summary>
-    void Update()
+    void FixedUpdate()
     {  
-        moveBullet();
+        MoveBullet();
     }
 
     /// <summary>
     /// Moves the projectile in a 'forward' direction
     /// </summary>
-    private void moveBullet()
+    private void MoveBullet()
     {
         transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
     }
@@ -39,7 +42,8 @@ public class Bullet : MonoBehaviour
     /// <summary>
     /// Destroys/removes the bullet based on a given amount of time
     /// </summary>
-    private void destroyBullet(float timeInSeconds = 0)
+    /// <param name="timeInSeconds"></param>
+    private void DestroyBullet(float timeInSeconds = 0)
     {
         Destroy(gameObject, timeInSeconds);
     }
@@ -47,8 +51,9 @@ public class Bullet : MonoBehaviour
     /// <summary>
     /// Destroys the object on collision
     /// </summary>
+    /// <param name="col"></param>
     void OnTriggerEnter(Collider col)
     {
-        destroyBullet();
+        DestroyBullet();
     }
 }
