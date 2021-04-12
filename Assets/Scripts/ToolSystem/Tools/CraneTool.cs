8937f2bc8d8650914d65a6caa1b3d64b79b4c816
Thomas Linssen <t.linssen@live.nl>
2021-04-12 09:49:31 +0200

    crane setup code

---
+++ Assets/Scripts/ToolSystem/Tools/CraneTool.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using ToolSystem;
+using UnityEngine;
+
+
+public class CraneTool : Tool
+{
+
+    float liftingProcess;
+    
+
+    private void HandleLifting()
+    {
+
+    }
+
+    private void CancelLifting()
+    {
+
+    }
+
+    private void StartLiftingObject()
+    {
+
+    }
+
+    private void GetLiftObjective()
+    {
+
+    }
+
+    public override void MoveTarget(Vector3 location)
+    {
+    }
+
+    public override void UseLeftAction(float pressedValue)
+    {
+    }
+
+    public override void UseRightAction(float pressedValue)
+    {
+    }
+}
