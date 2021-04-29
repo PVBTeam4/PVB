089c6e1c3afbc20c8b29ed754200b3340a782c18
Jaimy Putter <jaimy.putter@outlook.com>
2021-04-29 16:03:10 +0200

    Commented SceneAssetObject

--- Assets/Scripts/SceneSystem/SceneAssetObject.cs
+++ Assets/Scripts/SceneSystem/SceneAssetObject.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 
 namespace SceneSystem
 {
+    /// <summary>
+    /// Object used to hold Scene Info
+    /// </summary>
     [CreateAssetMenu(fileName = "New SceneAssetObject", menuName = "SceneAssetObject")]
     public class SceneAssetObject : ScriptableObject
     {
-        public string SceneName;
-        public ToolType ToolType;
+        // Scene name
+        [SerializeField]
+        private string sceneName;
+        
+        // ToolType of scene
+        [SerializeField]
+        private ToolType toolType;
+        
+        public string SceneName
+        {
+            get => sceneName;
+            private set => sceneName = value;
+        }
+        
+        public ToolType ToolType
+        {
+            get => toolType;
+            private set { this.toolType = value; }
+        }
     }
 }
