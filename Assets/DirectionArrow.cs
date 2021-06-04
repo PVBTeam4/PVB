7ae7bf3771f2c3f68a32fdb0cdcb1e0c19ae6165
Thomas Linssen <t.linssen@live.nl>
2021-06-04 16:35:47 +0200

    Removed unnecessary code

--- Assets/DirectionArrow.cs
+++ Assets/DirectionArrow.cs
@@ -14,11 +14,9 @@ public class DirectionArrow : MonoBehaviour
     {
         Vector3 distanceVector = targetObject.transform.position - thisObject.transform.position;
         float distance = distanceVector.magnitude;
-
-        float zPosDifference = targetObject.transform.position.z - thisObject.transform.position.z;
         float xPosDifference = targetObject.transform.position.x - thisObject.transform.position.x;
 
-        float zAngle = Mathf.Asin(xPosDifference / distance);//Mathf.Atan(xPosDifference / zPosDifference);
+        float zAngle = Mathf.Asin(xPosDifference / distance);
         zAngle *= Mathf.Rad2Deg;
         
         transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -zAngle + 90);
