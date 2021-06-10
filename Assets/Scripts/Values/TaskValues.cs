7219a0008539c4862002d3cb008d0e3cdfb1a2fc
MinneTW <Minneishappy@hotmail.com>
2021-06-10 10:17:47 +0200

    Tweaks

--- Assets/Scripts/Values/TaskValues.cs
+++ Assets/Scripts/Values/TaskValues.cs
@@ -2,7 +2,7 @@ namespace Values
 {
     public static class TaskValues
     {
-        public static readonly int RefugeeShipsToSave = 5;
+        public static readonly int RefugeeShipsToSave = 2;
         public static int RefugeeShipsKilled = 0;
         public static int RefugeeShipsSaved = 0;
     }
